62d36f196a7684783ea9b829fb42828e24d35782
Rati <goginashvilirati@gmail.com>
2024-08-30 17:56:01 +0200

    trying to make card loading work

--- src/assets/js/Cards.js
+++ src/assets/js/Cards.js
@@ -21,7 +21,8 @@ function displayCards(cardsData) {
             cardElement.className = 'card';
 
             const imgElement = document.createElement('img');
-            imgElement.src = card['Image Link'];
+            imgElement.dataset.src = card['Image Link'];
+            imgElement.className = 'lazy';
             cardElement.appendChild(imgElement);
 
             const contentElement = document.createElement('div');
@@ -41,6 +42,26 @@ function displayCards(cardsData) {
             container.appendChild(cardElement);
         }
     }
+    lazyLoadImages();
+}
+
+function lazyLoadImages() {
+    const lazyImages = document.querySelectorAll('img.lazy');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                img.src = img.dataset.src;
+                img.classList.remove('lazy');
+                observer.unobserve(img);
+            }
+        });
+    });
+
+    lazyImages.forEach(img => {
+        observer.observe(img);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
