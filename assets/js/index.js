0a96487ca74d2e7b8e8c0fcdb2b7f45dd16ad607
hatsuno <hatsunohana@gmail.com>
2024-09-04 16:37:08 +0200

    added back ground for the buttons container, added loading screen to the index page

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,3 +19,20 @@ function redirectToJuval() {
 function redirectToErvenia() {
     window.location.href = "Ervenia.html";
 }
+
+
+//page loader code//
+
+window.addEventListener('load', function() {
+    const loader = document.querySelector('.page_loader');
+
+    // Delay the animation by 2 seconds
+    setTimeout(function() {
+        loader.classList.add('slide-up');
+
+        // Optional: Remove the loader from the DOM after animation ends
+        loader.addEventListener('transitionend', function() {
+            loader.style.display = 'none';
+        });
+    }, 2000); // 2000 milliseconds = 2 seconds
+});
