62a773d387cbd36354f6eb89be93a998616ca4aa
Rati <goginashvilirati@gmail.com>
2024-09-28 10:40:10 +0200

    name is kinda done, but ervenian cards wont work out

--- assets/js/Creator.js
+++ assets/js/Creator.js
@@ -9,20 +9,26 @@ function createCard(){
     const kingdom = document.getElementById('card-kingdom').value;
     const type = document.getElementById('card-type').value;
     const rarity = document.getElementById('card-rarity').value;
+    const cardName = document.getElementById('card-name').value;
     const preview = document.getElementById('preview');
 
     // Clear previous card template
-    preview.innerHTML = '<div id="user-img"></div>';
+    preview.innerHTML = '<div id="user-img"></div><p id="card-name-display"></p>';
 
     // Create card template based on selected options
     const cardTemplate = document.createElement('img');
     cardTemplate.src = `../assets/img/card art/${kingdom}/${type}/${kingdom}-${type}Card-${rarity}.png`;
     cardTemplate.className = 'card-template';
     preview.appendChild(cardTemplate);
+
+    // Display card name
+    const cardNameDisplay = document.getElementById('card-name-display');
+    cardNameDisplay.textContent = cardName;
+    cardNameDisplay.className = 'card-name';
 }
 
 function uploadImage(e){
-    const file = event.target.files[0];
+    const file = e.target.files[0];
     const reader = new FileReader();
 
     reader.onload = function(e) {
