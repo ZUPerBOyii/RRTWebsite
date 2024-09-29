fd00c4385936b129ba21ef879856a67f5cf0b5b8
Rati <goginashvilirati@gmail.com>
2024-09-29 17:45:36 +0200

    idk trying shit out and seeing what works

--- assets/js/Creator.js
+++ assets/js/Creator.js
@@ -5,11 +5,15 @@ function init() {
     document.getElementById('card-image').addEventListener('change', uploadImage);
 }
 
-function createCard(){
+function createCard() {
     const kingdom = document.getElementById('card-kingdom').value;
     const type = document.getElementById('card-type').value;
     const rarity = document.getElementById('card-rarity').value;
     const cardName = document.getElementById('card-name').value;
+    const atk = document.getElementById('card-attack').value;
+    const def = document.getElementById('card-defense').value;
+    const mana = document.getElementById('card-mana').value;
+    const description = document.getElementById('card-description').value;
     const preview = document.getElementById('preview');
 
     // Clear previous card template
@@ -25,13 +29,32 @@ function createCard(){
     const cardNameDisplay = document.getElementById('card-name-display');
     cardNameDisplay.textContent = cardName;
     cardNameDisplay.className = 'card-name';
+
+    // Display card stats
+    const atkDisplay = document.getElementById('atk');
+    const defDisplay = document.getElementById('def');
+    const manaDisplay = document.getElementById('mana');
+    atkDisplay.textContent = atk;
+    atkDisplay.className = 'stat';
+    defDisplay.textContent = def;
+    defDisplay.className = 'stat';
+    manaDisplay.textContent = mana;
+    manaDisplay.className = 'stat';
+
+    // Display card description
+    const descriptionDisplay = document.createElement('p');
+    descriptionDisplay.textContent = description;
+    descriptionDisplay.className = 'card-description';
+    preview.appendChild(descriptionDisplay);
+
+
 }
 
-function uploadImage(e){
+function uploadImage(e) {
     const file = e.target.files[0];
     const reader = new FileReader();
 
-    reader.onload = function(e) {
+    reader.onload = function (e) {
         const userImg = document.getElementById('user-img');
         userImg.style.backgroundImage = `url(${e.target.result})`;
     };
